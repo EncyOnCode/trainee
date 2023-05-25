8a3e8387577e1e814b3d84af5ae94b63fb3d5565
CircleDev <metheenclosed@yandex.ru>
2023-05-25 21:48:09 +0400

    Added Hot Sales & Best seller

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,33 @@ class StartScreen extends StatelessWidget {
         centerTitle: true,
         title: const GPSWidget(),
       ),
-      body: Column(
+      body: ListView(
+        scrollDirection: Axis.vertical,
         children: [
-          CategoriesText(bigTitle: 'Select Category', smallTitle: 'view all', onTap: () {},),
+          CategoriesText(
+            bigTitle: 'Select Category',
+            smallTitle: 'view all',
+            onTap: () {},
+          ),
           const SizedBox(height: 10),
           const CategoryTextWidget(),
           const SizedBox(height: 20),
           const SearchBar(),
           const SizedBox(height: 20),
-          CategoriesText(bigTitle: 'Hot sales', smallTitle: 'see more', onTap: () {},)
+          CategoriesText(
+            bigTitle: 'Hot sales',
+            smallTitle: 'see more',
+            onTap: () {},
+          ),
+          const SizedBox(height: 20),
+          const HotSalesWidget(isNew: true),
+          const SizedBox(height: 20),
+          CategoriesText(
+            bigTitle: 'Best seller',
+            smallTitle: 'see more',
+            onTap: () {},
+          ),
+          PhoneGridWidget(),
         ],
       ),
     );
@@ -334,4 +352,294 @@ class _QRCodeButton extends StatelessWidget {
       ),
     );
   }
+}
+
+class HotSalesWidget extends StatefulWidget {
+  const HotSalesWidget({Key? key, required this.isNew}) : super(key: key);
+
+  final bool isNew;
+
+  @override
+  _HotSalesWidgetState createState() => _HotSalesWidgetState();
+}
+
+class _HotSalesWidgetState extends State<HotSalesWidget> {
+  final List<String> imageUrls = [
+    'https://media.discordapp.net/attachments/988860107423100938/1111314904872386662/image.png',
+    'https://media.discordapp.net/attachments/988860107423100938/1111314904872386662/image.png',
+    'https://media.discordapp.net/attachments/988860107423100938/1111314904872386662/image.png',
+  ];
+
+  final List<String> pageTitles = [
+    'Iphone 12',
+    'Some other title',
+    'Another title'
+  ];
+
+  final List<String> pageDescription = [
+    'Súper. Mega. Rápido.',
+    'Some other description',
+    'Another description'
+  ];
+
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 182,
+      child: PageView.builder(
+        itemCount: imageUrls.length,
+        onPageChanged: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    imageUrls[index],
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 8,
+                    left: 8,
+                    child: Container(
+                      width: 27,
+                      height: 27,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFFF6E4E),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'New',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 60,
+                    left: 8,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.black.withOpacity(0),
+                      child: Text(
+                        pageTitles[index],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 90,
+                    left: 8,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.black.withOpacity(0),
+                      child: Text(
+                        pageDescription[index],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 11,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 120,
+                    left: 8,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.black.withOpacity(0),
+                      child: TextButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                          padding: MaterialStatePropertyAll(
+                            EdgeInsets.symmetric(horizontal: 25),
+                          ),
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.white),
+                        ),
+                        child: const Text(
+                          'Buy now!',
+                          style: TextStyle(
+                            color: Color(0xFF010035),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PhoneGridWidget extends StatelessWidget {
+  final List<PhoneItem> phones = [
+    PhoneItem(
+      image:
+          'https://media.discordapp.net/attachments/988860107423100938/1111334844522303528/image.png',
+      currentPrice: '1,047',
+      previousPrice: '1,500',
+      name: 'Samsung Galaxy s20 Ultra',
+    ),
+    PhoneItem(
+      image:
+          'https://media.discordapp.net/attachments/988860107423100938/1111334909223645285/image.png',
+      currentPrice: '300',
+      previousPrice: '400',
+      name: 'Xiaomi Mi 10 Pro',
+    ),
+    PhoneItem(
+      image:
+          'https://media.discordapp.net/attachments/988860107423100938/1111334933055672390/image.png',
+      currentPrice: '1,047',
+      previousPrice: '1500',
+      name: 'Samsung Note 20 Ultra',
+    ),
+    PhoneItem(
+      image:
+          'https://media.discordapp.net/attachments/988860107423100938/1111334967562215484/image.png',
+      currentPrice: '300',
+      previousPrice: '400',
+      name: 'Motorola One Edge',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 472,
+      child: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: phones.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 0.75,
+        ),
+        itemBuilder: (context, index) {
+          final phone = phones[index];
+          return PhoneCard(
+            image: phone.image,
+            currentPrice: phone.currentPrice,
+            previousPrice: phone.previousPrice,
+            name: phone.name,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PhoneCard extends StatelessWidget {
+  final String image;
+  final String currentPrice;
+  final String previousPrice;
+  final String name;
+
+  const PhoneCard({
+    Key? key,
+    required this.image,
+    required this.currentPrice,
+    required this.previousPrice,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              image,
+              fit: BoxFit.cover,
+              height: 150,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    '\$$currentPrice',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF010035),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      '\$$previousPrice',
+                      style: const TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.lineThrough,
+                        color: Color(0xFFCCCCCC),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12, top: 4),
+              child: Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 10,
+                  color: Color(0xFF010035),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhoneItem {
+  final String image;
+  final String currentPrice;
+  final String previousPrice;
+  final String name;
+
+  PhoneItem({
+    required this.image,
+    required this.currentPrice,
+    required this.previousPrice,
+    required this.name,
+  });
 }
